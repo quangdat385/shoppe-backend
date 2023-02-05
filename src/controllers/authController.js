06ce4d55134a961feff55bb9594aea75d05ebc32
quangdat385 <95111284+quangdat385@users.noreply.github.com>
2023-02-05 22:29:10 +0700

    update 5/2/2023

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -122,122 +122,115 @@ class AuthController {
         
     }        
     //router PUT auth/:id/update/login?method=key(key get from responsive)
-    updateAndLogin(req, res, next){
-        (async ()=> {
-            
-            
-            
-            const {id,method,password} = req.body;
-            console.log(method,id,password);
-            if(!id||!password) {
-                return res.status(401).json({ message: 'All fields are required'});
-            };
-            const hashedPwd=await bcrypt.hash(password,10)
+    async updateAndLogin(req, res, next){
+        const {id,method,password} = req.body;
+        console.log(method,id,password);
+        if(!id||!password) {
+            return res.status(401).json({ message: 'All fields are required'});
+        };
+        const hashedPwd=await bcrypt.hash(password,10)
 
-            if(!hashedPwd) {
-                return res.status(403).json({ message: 'Password not hash' })
-            }
-            
-            await Users.findByIdAndUpdate({_id:id},{
-                password:hashedPwd,
-                isVerified:true
-            })
-                .then((user) => {
-                    const accessToken=createToken({
-                        "user":{
-                            "UserId":user._id,
-                            "roles":user.roles,
-                            "user_name":user.user_name
-                        }
-                    },process.env.ACCESS_TOKEN_SECRET,"15m");
-    
-                    const refreshToken=createToken({
-                        "user":{
-                            "userId":user._id
-                        }
-                    },process.env.REFRESH_TOKEN_SECRET,"7d");
-                    res.cookie('jwt',refreshToken,{
-                        httpOnly: true, //accessible only by web server 
-                        secure:true, //https
-                        sameSite: 'None', //cross-site cookie 
-                        maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match r
-                    });
-                    res.cookie('method_login',`${method}`,{   
-                        httpOnly: true, //accessible only by web server 
-                        secure:true, //https
-                        sameSite: 'None', //cross-site cookie 
-                        maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match r
-                    });
-                    res.status(201).json({
-                        type: "success",
-                        next_step:"views",
-                        message: "Login successfully",
-                        accessToken,
-                        
-                    });
-                })
-                .catch((err) => {
-                    res.status(401).json({ message: 'Unauthorized' })
-                    next(err)
-                });    
-            }
+        if(!hashedPwd) {
+            return res.status(403).json({ message: 'Password not hash' })
+        }
+        
+        await Users.findByIdAndUpdate({_id:id},{
+            password:hashedPwd,
+            isVerified:true
+        })
+            .then((user) => {
+                const accessToken=createToken({
+                    "user":{
+                        "UserId":user._id,
+                        "roles":user.roles,
+                        "user_name":user.user_name
+                    }
+                },process.env.ACCESS_TOKEN_SECRET,"15m");
 
-        )()    
-    }
-    //route POST auth/login
-    login(req, res){
-        (async ()=>{
-            const {phone_number,user_name,email,google,facebook,password}=req.body;
-
-            const method_login=phone_number&&{phone_number:phone_number}||email&&{email:email}
-                                ||google&&{google:google}||facebook&&{facebook:facebook}||user_name&&{user_name:user_name};
-            
-            if (!method_login|| !password) {
-                return res.status(400).json({message:'All fields are required' })
-            }
-            const key=Object.keys(method_login)[0];
-            const value=Object.values(method_login)[0];
-
-            const foundUser= await Users.findOne({[key]:value}).exec()
-            if(!foundUser||!foundUser.isVerified){
-                return res.status(401).json({message: 'Unauthorized'})
-            }
-            const match= await bcrypt.compare(password,foundUser.password)
-            if(!match) return res.status(401).json({message: 'Password mismatch'})
-            
-            const accessToken=createToken({
-                "user":{
-                    "UserId":foundUser._id,
-                    "roles":foundUser.roles,
-                    "user_name":foundUser.user_name
-                }
-            },process.env.ACCESS_TOKEN_SECRET,"15m");
-
-            const refreshToken=createToken({
-                "user":{
-                    "userId":foundUser._id,
+                const refreshToken=createToken({
+                    "user":{
+                        "userId":user._id
+                    }
+                },process.env.REFRESH_TOKEN_SECRET,"7d");
+                res.cookie('jwt',refreshToken,{
+                    httpOnly: true, //accessible only by web server 
+                    secure:true, //https
+                    sameSite: 'None', //cross-site cookie 
+                    maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match r
+                });
+                res.cookie('method_login',`${method}`,{   
+                    httpOnly: true, //accessible only by web server 
+                    secure:true, //https
+                    sameSite: 'None', //cross-site cookie 
+                    maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match r
+                });
+                res.status(201).json({
+                    type: "success",
+                    next_step:"views",
+                    message: "Login successfully",
+                    accessToken,
                     
-                }
-            },process.env.REFRESH_TOKEN_SECRET,"7d");
-            res.cookie('jwt',refreshToken,{
-                httpOnly: true, //accessible only by web server 
-                secure:true, //https
-                sameSite: 'None', //cross-site cookie 
-                maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match r
-            });
-            res.cookie('method_login',`${key}`,{   
-                httpOnly: true, //accessible only by web server 
-                secure:true, //https
-                sameSite: 'None', //cross-site cookie 
-                maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match r
-            });
-            res.status(201).json({
-                type: "success",
-                next_step:"views",
-                message: "User Login Successfully",
-                accessToken,
-            }) 
-        })()
+                });
+            })
+            .catch((err) => {
+                res.status(401).json({ message: 'Unauthorized' })
+                next(err)
+            });    
+    }
+        
+    
+    //route POST auth/login
+    async login(req, res){
+        const {phone_number,user_name,email,google,facebook,password}=req.body;
+
+        const method_login=phone_number&&{phone_number:phone_number}||email&&{email:email}
+                            ||google&&{google:google}||facebook&&{facebook:facebook}||user_name&&{user_name:user_name};
+        
+        if (!method_login|| !password) {
+            return res.status(400).json({message:'All fields are required' })
+        }
+        const key=Object.keys(method_login)[0];
+        const value=Object.values(method_login)[0];
+
+        const foundUser= await Users.findOne({[key]:value}).exec()
+        if(!foundUser||!foundUser.isVerified){
+            return res.status(401).json({message: 'Unauthorized'})
+        }
+        const match= await bcrypt.compare(password,foundUser.password)
+        if(!match) return res.status(401).json({message: 'Password mismatch'})
+        
+        const accessToken=createToken({
+            "user":{
+                "UserId":foundUser._id,
+                "roles":foundUser.roles,
+                "user_name":foundUser.user_name
+            }
+        },process.env.ACCESS_TOKEN_SECRET,"15m");
+
+        const refreshToken=createToken({
+            "user":{
+                "userId":foundUser._id,
+                
+            }
+        },process.env.REFRESH_TOKEN_SECRET,"7d");
+        res.cookie('jwt',refreshToken,{
+            httpOnly: true, //accessible only by web server 
+            secure:true, //https
+            sameSite: 'None', //cross-site cookie 
+            maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match r
+        });
+        res.cookie('method_login',`${key}`,{   
+            httpOnly: true, //accessible only by web server 
+            secure:true, //https
+            sameSite: 'None', //cross-site cookie 
+            maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match r
+        });
+        res.status(201).json({
+            type: "success",
+            next_step:"views",
+            message: "User Login Successfully",
+            accessToken,
+        }) 
     }
     //route GET auth/refresh
     refreshToken(req, res) {
@@ -282,90 +275,83 @@ class AuthController {
         res.json({ message:'Cookie cleared'})
     }
     //router POST auth/confirm
-    confirm(req, res){
-        (async ()=> {
-            const {phone_number,email}=req.body;
+    async confirm(req, res){
+        const {phone_number,email}=req.body;
 
-            if(!phone_number ){
-                return res.status(400).json({message:'Missing phone number or email'}) ;
-            }
+        if(!phone_number ){
+            return res.status(400).json({message:'Missing phone number or email'}) ;
+        }
 
-            const confirm_method=phone_number&&{phone_number:phone_number}||email&&{email:email};
-            const key= Object.keys(confirm_method)[0];
-            const value=Object.values(confirm_method)[0];
-            const user=await Users.findOne({[key]:value}).exec()
-            
-            if (!user||!user?.isVerified) {
-                res.status(401).json({message: 'Unauthorized' })
-            }else {
-                res.status(200).json({
-                    userId:user.id,
-                    phone_number:user.phone_number,
-                })
-            }
-            
-            
-        })()
+        const confirm_method=phone_number&&{phone_number:phone_number}||email&&{email:email};
+        const key= Object.keys(confirm_method)[0];
+        const value=Object.values(confirm_method)[0];
+        const user=await Users.findOne({[key]:value}).exec()
         
-        
+        if (!user||!user?.isVerified) {
+            res.status(401).json({message: 'Unauthorized' })
+        }else {
+            res.status(200).json({
+                userId:user.id,
+                phone_number:user.phone_number,
+            })
+        }
+            
     }
     //put auth/forgot/password
-    forgotPassword(req, res, next){
-        (async ()=> {
-            const {id,method,password}=req.body;
+    async forgotPassword(req, res, next){
+        const {id,method,password}=req.body;
         if(!id||!method||!password) {
-            return res.status(400).json({ message: 'All fields are required'});
+        return res.status(400).json({ message: 'All fields are required'});
         };
         const hashedPwd=await bcrypt.hash(password,10)
 
         if(!hashedPwd) {
-            return res.status(403).json({ message: 'Password not hash' })
+        return res.status(403).json({ message: 'Password not hash' })
         }
-        
+    
         await Users.findByIdAndUpdate({_id:id},{
             password:hashedPwd,
         })
-            .then((user) => {
-                
+        .then((user) => {
+            
 
-                const accessToken=createToken({
-                    "user":{
-                        "UserId":user._id,
-                        "roles":user.roles,
-                        "user_name":user.user_name
-                    }
-                },process.env.ACCESS_TOKEN_SECRET,"15m");
-    
-                const refreshToken=createToken({
-                    "user":{
-                        "userId":user._id,
-                        
-                    }
-                },process.env.REFRESH_TOKEN_SECRET,"7d");
-                res.cookie('jwt',refreshToken,{
-                    httpOnly: true, //accessible only by web server 
-                    secure: true, //https
-                    sameSite: 'None', //cross-site cookie 
-                    maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match r
-                });
-                res.cookie('method_login',`${method}`,{   
-                    httpOnly: true, //accessible only by web server 
-                    secure:true, //https
-                    sameSite: 'None', //cross-site cookie 
-                    maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match r
-                });
-                res.status(201).json({
-                    type: "success",
-                    next_step:"views",
-                    message: "Login successfully",
-                    accessToken
-                });
-            })
-            .catch((err) => {
-                res.status(401).json({ message: 'Unauthorized' })
-                next(err)
-            });   
-        })()
+            const accessToken=createToken({
+                "user":{
+                    "UserId":user._id,
+                    "roles":user.roles,
+                    "user_name":user.user_name
+                }
+            },process.env.ACCESS_TOKEN_SECRET,"15m");
+
+            const refreshToken=createToken({
+                "user":{
+                    "userId":user._id,
+                    
+                }
+            },process.env.REFRESH_TOKEN_SECRET,"7d");
+            res.cookie('jwt',refreshToken,{
+                httpOnly: true, //accessible only by web server 
+                secure: true, //https
+                sameSite: 'None', //cross-site cookie 
+                maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match r
+            });
+            res.cookie('method_login',`${method}`,{   
+                httpOnly: true, //accessible only by web server 
+                secure:true, //https
+                sameSite: 'None', //cross-site cookie 
+                maxAge: 7 * 24 * 60 * 60 * 1000 //cookie expiry: set to match r
+            });
+            res.status(201).json({
+                type: "success",
+                next_step:"views",
+                message: "Login successfully",
+                accessToken
+            });
+        })
+        .catch((err) => {
+            res.status(401).json({ message: 'Unauthorized' })
+            next(err)
+        });   
     }
     
     loginOtp(req, res, next){
